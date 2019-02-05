3dd4cdb51a800993110fd58258b864133f38200f
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2019-02-06 02:03:37 +0700

    [Updated]: handle 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,41 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from '@/views/Home';
-const Contact = { template: '<p>contact page</p>' };
-const Policy = { template: '<p>policy page</p>' };
-import Login from '@/views/Login';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import Vuex from 'vuex'
+const Contact = { template: '<p>contact page</p>' }
+const Policy = { template: '<p>policy page</p>' }
+const NotFoundComponent = { template: '<p>This page cannot be found!</p>' }
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-	mode: 'history',
-	routes: [
-		{
-			path: '/policy',
-			name: 'Policy',
-			component: Policy
-		},
-		{
-			path: '/contact-us',
-			name: 'Policy',
-			component: Contact
-		},
-		{
-			path: '/',
-			name: 'HelloWorld',
-			component: Home
-		},
-		{
-			path: '/test',
-			name: 'Test',
-			component: Login
-		}
-	]
-});
+  mode: 'history',
+  routes: [
+    {
+      path: '/policy',
+      name: 'Policy',
+      component: Policy
+    },
+    {
+      path: '/contact-us',
+      name: 'Policy',
+      component: Contact
+    },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: Home
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Login
+    },
+    {
+      path: '*',
+      name: 'Page is not founded',
+      component: NotFoundComponent
+    }
+  ]
+})
