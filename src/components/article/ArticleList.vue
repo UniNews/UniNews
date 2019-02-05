fb8cdcf697f32801866e49273813e71338bf2a9c
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2019-02-06 02:05:53 +0700

    [Updated]: responsive card from Vuetify

--- src/components/article/ArticleList.vue
+++ src/components/article/ArticleList.vue
@@ -1,7 +1,101 @@
 <template>
-	<el-row :gutter="20">
+  <v-layout
+    row
+    wrap
+    justify-space-between
+  >
+    <v-flex
+      lg4
+      sm6
+    >
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn
+            flat
+            color="orange"
+          >Share</v-btn>
+          <v-btn
+            flat
+            color="orange"
+          >Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+
+    <v-flex
+      lg4
+      sm6
+    >
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn
+            flat
+            color="orange"
+          >Share</v-btn>
+          <v-btn
+            flat
+            color="orange"
+          >Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+
+    <v-flex
+      lg4
+      sm6
+    >
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn
+            flat
+            color="orange"
+          >Share</v-btn>
+          <v-btn
+            flat
+            color="orange"
+          >Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+  <!-- <el-row :gutter="20">
 		<el-col :span="8" v-for="data in articles">
-					<!-- {{getAvgRating(data.rating)}} -->
 
 			<el-card :body-style="{ padding: '0px' }" shadow="hover">
 				<el-carousel style="height:200%" :autoplay="false" trigger="click" height="280px">
@@ -22,7 +116,7 @@
 				</div>
 			</el-card>
 		</el-col>
-	</el-row>
+	</el-row> -->
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,9 @@
   <div>
     <Quote v-if="!isAuthenticated"> </Quote>
 
-    <div class="hotNews">
-      <p class="text">
-        <i class="el-icon-loading"></i> Hot
-        <span>News</span>
-      </p>
+    <ArticleList> </ArticleList>
 
-      <ArticleList> </ArticleList>
-
-    </div>
+  </div>
 
   </div>
 </template>
@@ -29,19 +23,3 @@ export default {
 	}
 };
 </script>
-
-<style>
-.hotNews {
-	padding-top: 4%;
-}
-
-.hotNews p.text {
-	text-align: center;
-	font-size: 40px;
-	padding-bottom: 1%;
-}
-
-.hotNews p.text span {
-	color: rgb(255, 170, 11);
-}
-</style>
